41fcbcff886bbb82b879502c874394d118f1e6d3
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-02-12 12:02:45 +0100

    feat: implemented layer.py

--- layer.py
+++ layer.py
@@ -1,9 +1,19 @@
+from neuron import Neuron
 
 class Layer:
     def __init__(self, weights_list : list[list[float]], biases_list : list[float]):
         self.weights_list = weights_list
         self.biases_list = biases_list
+        self.neuron_list = []
+
+        for i in range(len(weights_list)):
+            self.neuron_list.append(Neuron(self.weights_list[i], self.biases_list[i]))
 
 
-    def forward(self, input_list : list[float]) -> float:
+    def forward(self, input_list : list[float]) -> list[float]:
+        layer_output = []
+        for neuron in self.neuron_list:
+            layer_output.append(neuron.forward(input_list))
+        
+        return layer_output
         
